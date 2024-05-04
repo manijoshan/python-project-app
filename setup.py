eb8736c9a42eb72e20425258d882ee94a4eee6f8
sairajuser <user@gmail.com>
2024-05-04 11:41:14 +0000

    added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+  name='johnalberto_flask_app',
+  version='1.0.0',
+  packages=find_packages(),
+  include_package_data=True,
+  py_modules=['main'], 
+  install_requires=[
+    'Flask>=3.0.3',
+  ],
+)
+
+
